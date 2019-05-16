8594a146d512fb56715881488a6e24cab9303ed0
maciej.bolsega@gmail.com <maciej.bolsega@gmail.com>
2019-05-16 20:26:00 +0200

    Commands, Handlers etc.

---
+++ Passenger.Infrastructure/Handlers/Drivers/CreateDriverHandler.cs
@@ -0,0 +1,21 @@
+using System.Threading.Tasks;
+using Passenger.Infrastructure.Commands;
+using Passenger.Infrastructure.Commands.Drivers;
+using Passenger.Infrastructure.Services;
+
+namespace Passenger.Infrastructure.Handlers.Drivers
+{
+    public class CreateDriverHandler : ICommandHandler<CreateDriver>
+    {
+        private readonly IDriverService _driverService;
+        public CreateDriverHandler(IDriverService driverService)
+        {
+             _driverService = driverService;
+        }
+
+        public Task HandleAsync(CreateDriver command)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
